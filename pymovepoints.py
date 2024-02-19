9a3964bdb6d8a2d157c25f1e97cff0e354fab356
SecondLinger <67423584+secondlinger@users.noreply.github.com>
2024-02-19 17:09:15 +0900

    Make class

--- pymovepoints.py
+++ pymovepoints.py
@@ -1,14 +1,73 @@
 import math
-import cv2
-import numpy as np
-import tkinter as tk
-from tkinter import filedialog
 import os
 import cv2
 import numpy as np
-
 from ImageResizer import ImageResizer
 from fileselector import VideoFileSelector
+import pickle
+
+
+def get_filename_without_extension(path):
+    # Split the path to get the filename with extension
+    _, filename_with_extension = os.path.split(path)
+    # Split the filename from its extension
+    filename_without_extension, _ = os.path.splitext(filename_with_extension)
+    return filename_without_extension
+
+
+def SaveData():
+    global points_list_img1, points_list_img2, points_list_10met, points_list_10metInv, TenMeterLen, fovx, fovy, a, b, c, d, e
+    data_to_save = {
+        'points_list_img1': points_list_img1,
+        'points_list_img2': points_list_img2,
+        'points_list_10met': points_list_10met,
+        'points_list_10metInv': points_list_10metInv,
+        'TenMeterLen': TenMeterLen,
+        'fov_x': 10,  # Example value, replace with actual trackbar value
+        'fov_y': 10,  # Example value, replace with actual trackbar value
+        'a': a,
+        'b': b,
+        'c': c,
+        'd': d,
+        'e': e,
+    }
+
+    global video_path
+    filename = get_filename_without_extension(video_path)
+
+    #make directory if not exist
+    if not os.path.exists('data'):
+        os.makedirs('data')
+
+
+    with open("data/"+filename+'.pkl', 'wb') as file:
+        pickle.dump(data_to_save, file)
+
+def load_data_from_file():
+    data_to_save = {
+        'points_list_img1': [],
+        'points_list_img2': [],
+        'points_list_10met': [],
+        'points_list_10metInv': [],
+        'TenMeterLen': 0,
+        'fov_x': 10,  # Example value, replace with actual trackbar value
+        'fov_y': 10,  # Example value, replace with actual trackbar value
+        'a': a,
+        'b': b,
+        'c': c,
+        'd': d,
+        'e': e,
+    }
+
+    global video_path
+    filename = get_filename_without_extension(video_path)
+    filename = "data/" + filename + '.pkl'
+
+    try:
+        with open(filename, 'rb') as file:
+            return pickle.load(file)
+    except (FileNotFoundError, EOFError, pickle.UnpicklingError):
+        return data_to_save
 
 # Initialize lists to store coordinates
 points_list_img1 = []
@@ -16,6 +75,8 @@ points_list_img2 = []
 
 points_list_10met  = []
 points_list_10metInv  = []
+
+TenMeterLen = 0
 
 a = 0.1
 b = -0.2
@@ -158,7 +219,6 @@ def blend_images(transformed_img, img2, alpha=0.5):
 
         return blended_image
 
-
 def fov_to_intrinsic(fov_x, fov_y, width, height):
     # Convert FoV from degrees to radians
     fov_x_rad = math.radians(fov_x)
@@ -194,9 +254,11 @@ def adjust_fov(frame, fov_x, fov_y, img_size):
     adjusted_frame = adjusted_frame[y:y + h, x:x + w]
     return adjusted_frame
 
+# Calculate the distance between the two transformed points
+def calculate_distance(point1, point2):
+    return np.sqrt((point1[0] - point2[0])**2 + (point1[1] - point2[1])**2)
 
-def apply_perspective_transform_and_map_points(original_img,src_points , matrix):
-        # Transform src_points from target_img to original_img
+def apply_perspective_transform_and_map_points(original_img,src_points, matrix):
         transformed_points = []
         for pt in src_points:
             point_homogenous = np.array([*pt, 1]).reshape(-1, 1)
@@ -273,6 +335,27 @@ img2 = original_img2.copy()
 # Initialize video capture
 cap = cv2.VideoCapture(video_path)
 
+
+data_loaded = load_data_from_file()
+
+
+# Access your data, using get to provide default values if keys are missing
+points_list_img1 = data_loaded.get('points_list_img1', [])
+points_list_img2 = data_loaded.get('points_list_img2', [])
+# Continue for other data as necessary
+
+
+# Example of accessing trackbar values, providing default if not found
+trackbar_values = data_loaded.get('trackbar_values', {})
+fovx = trackbar_values.get('FOV X', 10)
+fovy = trackbar_values.get('FOV Y', 10)
+
+a = data_loaded.get('a', 0)
+b = data_loaded.get('b', 0)
+c = data_loaded.get('c', 0)
+d = data_loaded.get('d', 0)
+e = data_loaded.get('e', 0)
+
 #----------------------------------------------------------------------------------
 # Windows for display
 cv2.namedWindow('Image')
@@ -287,19 +370,19 @@ cv2.setMouseCallback('TenMeterRef', ImageFunctions.click_event_img3)
 cv2.setMouseCallback('OriginalToInv', ImageFunctions.click_event_img4)
 
 # Create trackbars for FOV adjustment
-cv2.createTrackbar('FOV X', 'Image', 10, 1000, TrackbarCallbacks.on_fov_change_x)  # Assuming FOV range from 0 to 180
-cv2.createTrackbar('FOV Y', 'Image', 10, 1000, TrackbarCallbacks.on_fov_change_y)  # Adjust the range as needed
+cv2.createTrackbar('FOV X', 'Image', fovx, 1000, TrackbarCallbacks.on_fov_change_x)  # Assuming FOV range from 0 to 180
+cv2.createTrackbar('FOV Y', 'Image', fovy, 1000, TrackbarCallbacks.on_fov_change_y)  # Adjust the range as needed
 
 # Create trackbars for FOV adjustment
-cv2.createTrackbar('A', 'Image', 0, 10000, TrackbarCallbacks.on_A)  # Assuming FOV range from 0 to 180
-cv2.createTrackbar('B', 'Image', 0, 10000, TrackbarCallbacks.on_B)  # Adjust the range as needed
-cv2.createTrackbar('C', 'Image', 0, 1000, TrackbarCallbacks.on_C)  # Assuming FOV range from 0 to 180
-cv2.createTrackbar('D', 'Image', 0, 1000, TrackbarCallbacks.on_D)  # Adjust the range as needed
-cv2.createTrackbar('E', 'Image', 0, 1000, TrackbarCallbacks.on_E)  # Assuming FOV range from 0 to 180
+cv2.createTrackbar('A', 'Image', int(a), 10000, TrackbarCallbacks.on_A)  # Assuming FOV range from 0 to 180
+cv2.createTrackbar('B', 'Image', int(b), 10000, TrackbarCallbacks.on_B)  # Adjust the range as needed
+cv2.createTrackbar('C', 'Image', int(c), 1000, TrackbarCallbacks.on_C)  # Assuming FOV range from 0 to 180
+cv2.createTrackbar('D', 'Image', int(d), 1000, TrackbarCallbacks.on_D)  # Adjust the range as needed
+cv2.createTrackbar('E', 'Image', int(e), 1000, TrackbarCallbacks.on_E)  # Assuming FOV range from 0 to 180
 
 #----------------------------------------------------------------------------------
 
-scale = 2
+scale = 1
 
 while True:
     ret, frame = cap.read()
@@ -321,12 +404,14 @@ while True:
     frame_img1 = cv2.resize(frame, (frame.shape[1]*scale, frame.shape[0]*scale))
     img2 = cv2.resize(img2, (img2.shape[1]*scale, img2.shape[0]*scale))
 
-    img2 = ImageResizer.ResizeWithAspectRatioAndFill(img2,width=1780, height=1000)  # Resize by width OR
+    img2 = ImageResizer.ResizeWithAspectRatioAndFill(img2,width=1780, height=1000)
 
-    k = 1.0
-    kernel = np.array([[-k, -k, -k], [-k, 1 + 8 * k, -k], [-k, -k, -k]])
-    frame_img1 = cv2.filter2D(frame_img1, ddepth=-1, kernel=kernel)
-    frame_img2 = cv2.filter2D(img2, ddepth=-1, kernel=kernel)
+   # k = 1.0
+    #kernel = np.array([[-k, -k, -k], [-k, 1 + 8 * k, -k], [-k, -k, -k]])
+    #frame_img1 = cv2.filter2D(frame_img1, ddepth=-1, kernel=kernel)
+    #frame_img2 = cv2.filter2D(img2, ddepth=-1, kernel=kernel)
+
+    frame_img2 = cv2.resize(img2, (img2.shape[1]*scale, img2.shape[0]*scale))
 
     frame_img1 = adjust_fov(frame_img1, fovx/10, fovy/10, (frame_img1.shape[1], frame_img1.shape[0]))
     original_frame_img1 = frame_img1.copy()
@@ -339,7 +424,12 @@ while True:
     InvImage, matrix = apply_perspective_transform()
 
     if matrix is not None:
-        apply_perspective_transform_and_map_points(original_frame_img1, points_list_10met, matrix)
+        apply_perspective_transform_and_map_points(original_frame_img1, points_list_10metInv, matrix)
+
+        if len(points_list_10metInv) == 2:
+            TenMeterLen = calculate_distance(points_list_10metInv[0], points_list_10metInv[1])
+            print(TenMeterLen)
+
         InvImage, detransformed_points = apply_inverse_perspective_transform_and_map_points(InvImage, points_list_10metInv, matrix)
 
     if InvImage is not None:
@@ -360,6 +450,9 @@ while True:
     key = cv2.waitKey(1) & 0xFF
     if key == ord('q'):
         break
+
+    if key == ord('s'):
+        SaveData()
 
 cap.release()
 cv2.destroyAllWindows()
